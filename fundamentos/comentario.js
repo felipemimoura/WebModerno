58c62956602f4dc84cd9a1df27da31bcf151e244
Felipe Moura <felipemimoura@gmail.com>
2020-09-02 15:41:01 -0300

    day 01

---
+++ fundamentos/comentario.js
@@ -0,0 +1,8 @@
+console.log('Uma linha'); //COmentario de uma unica linha
+
+/* Comentario
+ de
+  multiplas
+   linhas 
+*/
+console.log('multiplas linhas');
---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,22 @@
+/* Declaração de variaveis
+ a recebe o valor 3
+*/
+var a = 3;
+
+/* B recebe o valor  */
+let b = 4;
+
+console.log(a, b);
+/* Alteração dos valores e a e b */
+var a = 30; //é possivel, mas não é recomendado
+b = 400;
+
+console.log(a, b);
+
+a = 500;
+b = 600;
+console.log(a, b);
+
+const c = 1000; // const não se altera
+
+console.log(c);
