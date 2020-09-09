c93105966b392f277137297e74a59c7ce307e89f
Felipe Moura <felipemimoura@gmail.com>
2020-09-09 10:17:30 -0300

    secao estrutura de controle

---
+++ Control_structure/if2.js
@@ -0,0 +1,16 @@
+function teste1(num) {
+  if (num > 7) {
+    console.log(num);
+    console.log('final');
+  }
+}
+teste1(6);
+teste1(8);
+
+function teste2(num) {
+  if (num > 7);
+  {
+    //cuidado com o ponto e virgula nas estruturas de controle
+    console.log(num);
+  }
+}
