8ccc39da249b52ac2ef7e91f1f8022b4169a5295
Felipe Moura <felipemimoura@gmail.com>
2020-09-10 17:02:35 -0300

    secaoFuncao

---
+++ funcao/arrowFunction1.js
@@ -0,0 +1,15 @@
+let dobro = function (a) {
+  return a * 2;
+};
+dobro = (a) => {
+  return a * 2;
+};
+dobro = (a) => a * 2;
+console.log(dobro(Math.PI));
+
+let ola = function () {
+  return 'Ola';
+};
+ola = () => 'Olá';
+ola = (_) => 'Olá';
+console.log(ola());
